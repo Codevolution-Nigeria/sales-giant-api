a6bae0d34b5670d3da57e25d86b9fcd286ebaeca
abiodun-michael <michaemedewase@gmail.com>
2023-03-10 18:57:39 +0100

    chnaged origin

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors({
-    origin:["localhost:3000"],
+    origin:["http://localhost:3000"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     credentials: true,
